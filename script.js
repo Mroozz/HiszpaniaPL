b40f917af49efd4e5d909696ea717170e600ffb1
Mateusz <63881385+Mroozz@users.noreply.github.com>
2024-06-13 19:23:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAj_uOIUAbi-4k-6oSL05HJIbh8RPJ4fhs",
+    authDomain: "hiszpaniapl.firebaseapp.com",
+    projectId: "hiszpaniapl",
+    storageBucket: "hiszpaniapl.appspot.com",
+    messagingSenderId: "145561262253",
+    appId: "1:145561262253:web:76ecb75e7bccd428d84e24"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
