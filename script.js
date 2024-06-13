3d58e4221ada5acf2b777a2c2f66ad08128d8407
Mateusz <63881385+Mroozz@users.noreply.github.com>
2024-06-13 20:13:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBOdr5bDRwsZCXOjlX_TVXFNEnVA0w812Q",
-    authDomain: "esnieru.firebaseapp.com",
-    databaseURL: "https://esnieru-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "esnieru",
-    storageBucket: "esnieru.appspot.com",
-    messagingSenderId: "523836156102",
-    appId: "1:523836156102:web:5dc41e61790291edfa8fc7"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 // Get a reference to the database service
 const database = firebase.database();
 
